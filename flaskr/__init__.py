21a263a3714a33adefe5627c6f747919c1ef9bf3
Jonas Andriunaitis <andriunaitis@gmail.com>
2022-03-15 17:56:25 +0200

    add missing import

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,7 +1,7 @@
 import os
 import json
 from functools import wraps
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
