7907ef31dbc91553c3141299e930c6025b976aa3
Jonas Andriunaitis <andriunaitis@gmail.com>
2022-03-16 02:04:46 +0200

    add validation and more tests, rename test file

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -2,6 +2,25 @@ import os
 from functools import wraps
 from flask import Flask, jsonify, request, abort
 
+def validate_json(validators):
+    def decorate(f):
+        @wraps(f)
+        def wrapper(*args, **kwargs):
+            if isinstance(request.json, dict):
+                try:
+                    result = map(lambda valid: valid(request.json or {}), validators)
+                    errors = filter(lambda pair: pair[0], result)
+                    if any(errors):
+                        return jsonify({"errors": list(map(lambda pair: pair[1], errors))}), 400
+                    else:
+                        return f(*args, **kwargs)
+                except KeyError as e:
+                    return jsonify({"errors": ["Missing key {} in json object".format(e)]}), 400
+            else:
+                return jsonify({"errors": ["Malformed json"]}), 400
+        return wrapper
+    return decorate
+
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     
@@ -10,6 +29,23 @@ def create_app(test_config=None):
         return b"Hello world!"
 
     @app.route('/digits/sum', methods=(['POST']))
+    @validate_json([
+        lambda json: (
+            any(map(lambda v: type(v) != str, json["address"]["colorKeys"])),
+            "some of colorKeys were not type str"
+        ),
+        lambda json: (
+            any(map(lambda v: type(v) != int, json["address"]["values"])),
+            "some of values were not type int"
+        ),
+        lambda json: (
+            type(json["meta"]["digits"]) != int,
+            "digits should be type int"
+        ),
+        lambda json: (
+            type(json["meta"]["processingPattern"]) != str,
+            "processingPattern should be type str"),
+    ])
     def digits_sum():
         values_sum = sum(request.json["address"]["values"])
         digits_iter = map(int, str(values_sum))
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,8 +1,8 @@
 import json
 from flaskr import create_app
 
-def test_digit_sum_result_api(client):
-    """Should respond with result of digit sum from sum of values in test data"""
+def test_digits_sum_ok_result(client):
+    """Should respond with result of digits sum from sum of values in test data"""
     test_data = {
         "address": {
             "colorKeys": ["A", "G", "Z"],
@@ -17,18 +17,38 @@ def test_digit_sum_result_api(client):
     assert response.status_code == 200
     assert json.loads(response.data)["result"] == 8 
 
-def test_digit_sum_api_invalid_data(client):
+def test_digits_sum_invalid_data(client):
+    """Should respond with errors for each invalid field"""
+    test_data = {
+        "address": {
+            "colorKeys": [1, 2, 3],
+            "values": ["1", "2"]
+        },
+        "meta": {
+            "digits": True,
+            "processingPattern": None
+        }
+    }
+    response = client.post('/digits/sum', json=test_data)
+    assert response.status_code == 400
+
+def test_digits_sum_malformed_json(client):
     """Should respond with bad request when invalid json"""
     test_data = "{"
     response = client.post('/digits/sum', json=test_data)
     assert response.status_code == 400
 
-def test_digit_sum_api_missing_keys(client):
+def test_digits_sum_missing_json_keys(client):
     """Should respond with bad request when missing keys in json"""
     test_data = {
         "address": {},
         "meta": {}
     }
     response = client.post('/digits/sum', json=test_data)
+    assert response.status_code == 400
+    
+def test_digits_sum_missing_json_header(client):
+    """Should respond with bad request when request body is not json"""
+    response = client.post('/digits/sum', data="oh no")
     assert response.status_code == 400
     
